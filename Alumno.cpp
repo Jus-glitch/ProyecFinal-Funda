6a5876fb1888b69d7018753d118c2d37afe37cb8
Jus-glitch <jstnvalle3427@gmail.com>
2025-06-04 20:58:47 -0600

    Inicia1

--- Alumno.cpp
+++ Alumno.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
-#include <string>
-#include <iomanip>
 
-using namespace std;
 
-struct Alumno {
-    string primerNombre;        // Primer nombre del alumno
-    string primerApellido;      // Primer apellido del alumno
-    string segundoApellido;     // Segundo apellido del alumno
-    int cicloEstudio;           // Ciclo de estudio en el que se encuentra
-    int numeroCedula;           // Número de cédula del alumno
-    double* calificaciones;      // Puntero para almacenar las calificaciones
-    int numCalif;               // Número de calificaciones
-    double promedio;            // Promedio de las calificaciones
-}
+alumno.numCalif = numCalif;
+    alumno.promedio = 0.0;
+    alumno.calificaciones = new double[numCalif];
